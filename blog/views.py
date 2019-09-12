10a6af2816fddfe9a54bfeab6fdd2034d48f4c92
P0l0ny <54375959+P0l0ny@users.noreply.github.com>
2019-09-12 07:40:29 +0200

    Second update
    Few updates to database etc

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-#post details displayed on home.html
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 8, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 9, 2019'
-    }
-]
 
 #function to display info on home page
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context, {'title' : 'Home'})
    
